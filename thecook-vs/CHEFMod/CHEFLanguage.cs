51cc9a1b341d4eec75017d12308ba7416d6c14d8
DestroyedClone <destroyedclone@gmail.com>
2021-11-30 02:38:18 -0800

    Language migration

---
+++ thecook-vs/CHEFMod/CHEFLanguage.cs
@@ -0,0 +1,27 @@
+﻿using RoR2;
+using System.Collections.Generic;
+using Zio;
+using Zio.FileSystems;
+
+namespace ChefMod
+{
+    public static class CHEFLanguage
+    {
+        public static FileSystem FileSystem { get; private set; }
+
+        public static void Initialize()
+        {
+            PhysicalFileSystem physicalFileSystem = new PhysicalFileSystem();
+            FileSystem = new SubFileSystem(physicalFileSystem, physicalFileSystem.ConvertPathFromInternal(Assets.AssemblyDir), true);
+
+            if (FileSystem.DirectoryExists("/Languages/"))
+            {
+                Language.collectLanguageRootFolders += delegate (List<DirectoryEntry> list)
+                {
+                    ChefPlugin.logger.LogMessage($"Initializing Language");
+                    list.Add(FileSystem.GetDirectoryEntry("/Languages/"));
+                };
+            }
+        }
+    }
+}
